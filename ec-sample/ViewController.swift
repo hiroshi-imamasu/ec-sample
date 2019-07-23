6ad29026552d029cbc481c0867e3070b5e2da0e9
Hiroshi Imamasu <hiroshi.imamasu@repro.io>
2019-07-24 00:20:27 +0900

    Initial Commit

---
+++ ec-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ec-sample
+//
+//  Created by Trial: Hiroshi Imamasu on 2019/07/24.
+//  Copyright © 2019 Repro CRE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
